b1bfa9f3d208fe40e2f94f6a92ae4c855f0286e9
Reece W <me@reece.sx>
2019-06-18 12:10:29 +0100

    Plugin loader: use xlib csv api instead of the first nasty flawed poc

--- src/Core/core_loader.c
+++ src/Core/core_loader.c
@@ -15,7 +15,7 @@ static linked_list_head_p top_level_plugin_tracking;
 static error_t loader_init()
 {
     top_level_plugin_tracking = linked_list_create();
-    return !top_level_plugin_tracking ? kErrorOutOfMemory : kStatusOkay;
+    return !top_level_plugin_tracking ? XENUS_ERROR_OUT_OF_MEMORY : XENUS_OKAY;
 }
 
 static error_t loader_append(const char * name, const char * iat, const char * path)
@@ -29,51 +29,8 @@ static error_t loader_transfer()
 {
     plugins_initialize(top_level_plugin_tracking);
     plugins_start(top_level_plugin_tracking);
-    return kStatusOkay;
+    return XENUS_OKAY;
 }
-
-static error_t loader_read_csv_data(const char * buffer, uint_t buf_length, uint_t * offset, char * data, uint_t data_length)
-{
-#define MATCH_CHAR_SEPERATOR(c) (c == ' ' || c == ',' || c == '\r' || c == '\n' || c == '\x00')
-    uint_t end_index;
-    uint_t key_length;
-    uint_t copy_length;
-    uint_t start_index;
-
-    key_length    = 0;
-    start_index   = *offset;
-    end_index     = *offset;
-
-    // read until next seperator 
-    for (NULL; end_index < buf_length; end_index++)
-    {
-        char cur;
-        cur = buffer[end_index];
-
-        if (MATCH_CHAR_SEPERATOR(cur))
-            goto copy;
-
-        key_length++;
-    }
-copy:
-
-    // copy string and append null char
-    copy_length = MIN(data_length - 1, key_length);
-    memcpy(data, (const char *)(((size_t)buffer) + start_index), copy_length);
-    data[copy_length] = '\x00';
-
-    while (end_index < buf_length)
-    {
-        if (!(MATCH_CHAR_SEPERATOR(buffer[end_index])))
-            break;
-        end_index++;
-    }
-
-    *offset = end_index;
-    return kStatusOkay; 
-}
-
-
 
 static error_t loader_load_from_csv(const char * str, uint_t length)
 {
@@ -86,16 +43,27 @@ static error_t loader_load_from_csv(const char * str, uint_t length)
 
     while (true)
     {
-        loader_read_csv_data(str, length, &counter, name, sizeof(name));
-        if (strlen(name) == 0) break;
-        loader_read_csv_data(str, length, &counter, module, sizeof(module));
-        if (strlen(module) == 0) break;
-        loader_read_csv_data(str, length, &counter, path, sizeof(path));
-        if (strlen(path) == 0) break;
+        error_t err;
+
+        err = csv_process_buffer(str, length, &counter, name, sizeof(name), NULL);
+        if ((err == XENUS_ERROR_CSV_EOS) || (err == XENUS_STATUS_CSV_READ_ALL))
+            break;
+
+        err = csv_process_buffer(str, length, &counter, module, sizeof(module), NULL);
+        if ((err == XENUS_ERROR_CSV_EOS) || (err == XENUS_STATUS_CSV_READ_ALL))
+            break;
+
+        err = csv_process_buffer(str, length, &counter, path, sizeof(path), NULL);
+        if (err == XENUS_ERROR_CSV_EOS)
+            break;
+
         loader_append(name, module, path);
+
+        if (err == XENUS_STATUS_CSV_READ_ALL)
+            break;
     }
 
-    return kStatusOkay;
+    return XENUS_OKAY;
 }
 
 static error_t loader_load_from_file()
@@ -103,19 +71,22 @@ static error_t loader_load_from_file()
     const char * test = "LibOS, LibOS.XSYS, /Xenus/Kernel/LibOS.XSYS\n"
                         "SelfTestModule, SelfTest.XSYS, /Xenus/Kernel/Plugins/SelfTest.XSYS";
     loader_load_from_csv(test, 111);
-    return kStatusOkay;
+    return XENUS_OKAY;
 }
 
 void xenus_launch_plugins()
 {
     error_t err;
 
-    if (ERROR(err = loader_init())) 
+    err = loader_init();
+    if (ERROR(err)) 
         panicf("_loader_init failed with %lli", err);
 
-    if (ERROR(err = loader_load_from_file()))
+    err = loader_load_from_file();
+    if (ERROR(err))
         panicf("_loader_load_from_file failed with %lli", err);
 
-    if (ERROR(err = loader_transfer()))
+    err = loader_transfer();
+    if (ERROR(err))
         panicf("_loader_transfer failed with %lli", err);
 }
