c6fa5e8f07a6b03029482afe8f8e36dcbd04dac4
Reece W <me@reece.sx>
2019-07-18 05:34:20 +0100

    Use a mask instead of branch logic to realign the stack

--- src/StackFixup/stack_realigner.asm
+++ src/StackFixup/stack_realigner.asm
@@ -10,34 +10,27 @@ PUBLIC stack_realigner
 ;; note: your backtrace WILL end here. msvc and the debugging apis should identify this as external code 
 ;; QWORD(* stack_realigner)(QWORD(* callback)(void * context), void * context)
 stack_realigner PROC    
-    mov rax, rsp
-    ;;add rax, 8	   ;;   [example prolog:  push  R15		    - WE ARE HERE - NOT ALIGNED
-                       ;;                     push  R14
-                       ;;                     push  R13		    - POSTEXEC: 16 byte aligned
-                       ;;                     sub rsp, reserve; - POSTEXEC: 16 byte aligned	] 
-                       ;;
-    and rax, 15d       ;; 
-    cmp rax, 0         ;;  ZF = bit((rsp & 15) - 0) == 0) == bit(rsp mod 16 == 0)
-                      
-    je alreadyAlignedAsLeaf  ;; if (ZF) { alreadyAligned } else { notAligned }
-    notAligned:
-        sub rsp, 8  
-        sub rsp, 32d
-        mov rax, rcx
-        mov rcx, rdx
-        call rax
-        add rsp, 8
-        jmp cleanUp
+	push rbp
+	mov rbp, rsp
+	
+	and rsp, 0fffffffffffffff7h
+	sub rbp, rsp 
 
-    alreadyAlignedAsLeaf:
-        sub rsp, 32d
-        mov rax, rcx
-        mov rcx, rdx
-        call rax
-    cleanUp:
-        add rsp, 32
-        ret
+	;; mov rax, rsp
+	;; add rax, rbp
+	;; add rax, 8
+	;; rax = caller 
+	
+	mov rax, rcx
+	mov rcx, rdx
+	sub rsp, 32d
+	call rax
+	add rsp, 32d
+	
+	add rsp, rbp 
+	pop rbp
 
+	ret
 stack_realigner ENDP
 
 END
